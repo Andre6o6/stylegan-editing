42f56b069a8105e97133b7ba0028b2bd5a0599f7
Andrey Sugonyaev <34348146+Andre6o6@users.noreply.github.com>
2021-01-07 16:50:05 +0300

    Enable optimization in Z and W space

--- models/latent_optimization.py
+++ models/latent_optimization.py
@@ -4,35 +4,47 @@ import torch.nn.functional as F
 from torchvision.models import vgg16
 
 def denormalize(synthesized_image, min_value=-1, max_value=1):
-    #Cast from [-1, 1] to [0, 255]; gradients should be ok ###(?)###
-    synthesized_image = 255. * (synthesized_image - min_value) / (max_value - min_value)
-    synthesized_image = torch.clamp(synthesized_image + 0.5, min=0, max=255)
+    """Convert from range [min_value, max_value] to [0.0, 1.0]"""
+    synthesized_image = (synthesized_image - min_value) / (max_value - min_value)
+    synthesized_image = torch.clamp(synthesized_image + 0.5, min=0, max=1)
     return synthesized_image
 
-class VGGFeatureExtractor(nn.Module):
-    def __init__(self, vgg_layer=12):
+class VGGPrerocess(nn.Module):
+    def __init__(self, image_size=256):
         super().__init__()
-        self.image_size = 256
-        self.mean = torch.tensor([0.485, 0.456, 0.406]).to(device).view(-1, 1, 1)
-        self.std = torch.tensor([0.229, 0.224, 0.225]).to(device).view(-1, 1, 1)
-
-        self.vgg16 = vgg16(pretrained=True).features[:vgg_layer].to(device).eval()
+        self.image_size = image_size
+        self.mean = torch.tensor([0.485, 0.456, 0.406]).to(device).view(-1,1,1)
+        self.std = torch.tensor([0.229, 0.224, 0.225]).to(device).view(-1,1,1)
 
     def forward(self, image):
-        image = image / 255.
         image = F.adaptive_avg_pool2d(image, self.image_size)
         image = (image - self.mean) / self.std
-        features = self.vgg16(image)
-        return features
+        return image
 
 class LatentOptimizer(nn.Module):
-    def __init__(self, synthesizer, vgg_layer=9):
+    def __init__(self, model, vgg_layer=12, latent_space='WP'):
         super().__init__()
-        self.synthesizer = synthesizer.to(device).eval()
-        self.feature_extractor = VGGFeatureExtractor(vgg_layer)
+        
+        assert latent_space in ['Z', 'W', 'WP'], "Only Z, W, WP are supported"
+
+        self.model = model.to(device).eval()
+        self.latent_space = latent_space
+        self.preprocess = VGGPrerocess()
+        self.feature_extractor = vgg16(pretrained=True).features[:vgg_layer].to(device).eval()
 
     def forward(self, dlatents):
-        generated_image = self.synthesizer(dlatents)
+        if self.latent_space == 'Z':
+            zs = dlatents
+            ws = self.model.mapping(zs)
+            wps = self.model.truncation(ws)
+        elif self.latent_space == 'W':
+            ws = dlatents
+            wps = self.model.truncation(ws)
+        elif self.latent_space == 'WP':
+            wps = dlatents
+
+        generated_image = self.model.synthesis(wps)
         generated_image = denormalize(generated_image)
-        features = self.feature_extractor(generated_image)
+        preprocessed_image = self.preprocess(generated_image)
+        features = self.feature_extractor(preprocessed_image)
         return features, generated_image
