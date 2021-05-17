3efe9c103232eef4133f2a3b7e56a5617dc9bee0
Andrey Sugonyaev <sug.andre606@gmail.com>
2021-05-17 22:18:16 +0300

    feature transfer code

---
+++ edit_images.py
@@ -0,0 +1,157 @@
+"""This module implements Feature transfer using latent vectors from latent 
+optimization.
+"""
+import numpy as np
+import os
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from facenet_pytorch import InceptionResnetV1
+from interfacegan.models.stylegan_generator import StyleGANGenerator
+from models.latent_optimization import LatentOptimizer
+
+
+def morph_coefficient(w_input, w_exemplar, boundary, map_k=None):
+    if map_k is None:
+        proj_x = np.inner(w_input[:, 0], boundary)
+        proj_y = np.inner(w_exemplar[:, 0], boundary)
+    else:
+        proj_x = np.inner(np.mean(w_input[:, :map_k], axis=1), boundary)
+        proj_y = np.inner(np.mean(w_exemplar[:, :map_k], axis=1), boundary)
+    return proj_y[0,0] - proj_x[0,0]
+
+
+def feature_morph(latent, boundary, effect_coef, 
+                  n_iters=100, identity_correction=True, identity_coef=2):
+
+    latent = torch.tensor(latent, dtype=torch.float32, device=device)
+    latent.requires_grad_(True)
+
+    with torch.no_grad():
+        original_img = latent_optimizer.model.synthesis(latent)
+        original_emb = facenet(original_img)
+
+    boundary = torch.tensor(boundary, dtype=torch.float32, device=device)
+    boundary_expended = torch.tile(boundary, (1,18,1)).to(device)
+
+    path = []
+    for step in range(n_iters):
+        with torch.no_grad():
+            latent += boundary * (effect_coef/n_iters)
+        
+        if identity_correction:
+            if (latent.grad is not None):
+                latent.grad.zero_()
+            
+            new_img = latent_optimizer.model.synthesis(latent)
+            new_emb = facenet(new_img)
+            loss = torch.inner(original_emb, new_emb)
+            loss.backward()
+
+            with torch.no_grad():
+                gradients = latent.grad
+                
+                projection = torch.inner(gradients.squeeze(), boundary.squeeze())
+                projected_b = projection.view(-1,1)*boundary_expended.squeeze()
+                ord_gradients = gradients - projected_b
+
+                latent += identity_coef * ord_gradients
+        path.append(latent.detach().cpu().numpy())
+    
+    result = path[-1]
+    return result, path
+
+
+ def arg_parse():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Latent Optimization module")
+    parser.add_argument(
+        "--input",
+        dest="w_input",
+        help="Path to input image latent vector",
+        type=str,
+    )
+    parser.add_argument(
+        "--exemplar",
+        dest="w_exemplar",
+        help="Path to exemplar image latent vector",
+        type=str,
+    )
+    parser.add_argument(
+        "--boundary",
+        dest="boundary",
+        help="One of 'smile', 'pose' or path to boundary vector",
+        default="pose",
+        type=str,
+    )
+    parser.add_argument(
+        "--out",
+        dest="out_dir",
+        help="Results directory",
+        default="out/",
+        type=str,
+    )
+    parser.add_argument(
+        "--iters",
+        dest="n_iters",
+        help="Number of iterations",
+        default=100,
+        type=int,
+    )
+    parser.add_argument(
+        "--identity",
+        dest="identity",
+        help="Identity correction coefficient",
+        default=2.0,
+        type=float,
+    )
+    return parser.parse_args()   
+
+
+def main():
+    args = arg_parse()
+    identity_correction = args.identity > 0
+    
+    os.makedirs(args.out_dir, exist_ok=True)
+    
+    # Load latent vectors and boundary vector
+    w_input = np.load(args.w_input)
+    w_exemplar = np.load(args.w_exemplar)
+    
+    if (args.boundary.lower() == "smile"):
+        boundary = np.load("boundaries/stylegan_ffhq_smile_w_boundary.npy")
+    elif (args.boundary.lower() == "pose"):
+        boundary = np.load("boundaries/stylegan_ffhq_pose_w_boundary.npy")
+    else:
+        boundary = np.load(args.boundary)
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    # Load GAN generator
+    print("Loading StyleGAN, pretrained model should be in 'interfacegan/models/pretrain/'")
+    converted_model = StyleGANGenerator("stylegan_ffhq")
+    latent_optimizer = LatentOptimizer(converted_model.model, latent_space="WP")
+    
+    facenet = InceptionResnetV1(pretrained='vggface2').to(device).eval()
+    
+    # Feature transfer
+    effect_coef = morph_coefficient(w_input, w_exemplar, boundary)
+    result_latent, _ = feature_morph(w_input, boundary, effect_coef, \
+        n_iters=args.n_iters, identity_correction=identity_correction, identity_coef=args.identity)
+    
+    # Save results
+    latent_name = args.w_input.split('/')[-1]
+    np.save(os.path.join(args.out_dir, latent_name), result_latent)
+    
+    generated_image = converted_model.synthesize(result_latent, "WP")["image"]
+    generated_image = converted_model.postprocess(generated_image)[0]
+    img_pil = Image.fromarray(generated_image)
+    img_name = latent_name.rstrip(".npy")
+    img_pil.save(os.path.join(args.out_dir, img_name))
+    
+
+if __name__ == "__main__":
+    main()
--- encode_images.py
+++ encode_images.py
@@ -1,8 +1,13 @@
+"""This module implements Latent Optimization, aimed to project input images into
+StyleGAN's latent space.
+"""
+import argparse
 import cv2
 import numpy as np
 import os
 from PIL import Image
 from tqdm import tqdm
+from typing import Tuple
 
 import torch
 import torch.nn as nn
@@ -15,16 +20,18 @@ from models.initial_prediction import InitialLatentPredictor
 from models.loss import LatentLoss
 from utils.blur import GaussianSmoothing
 
-# Get target image tensor and target features
+
 def prepare_reference_image(imagepath):
+    """Get target image tensor and target features tensor."""
     reference_image = transforms.ToTensor()(Image.open(imagepath)).unsqueeze(0).to(device)
     with torch.no_grad():
         prep_image = latent_optimizer.preprocess(reference_image)
         reference_features = latent_optimizer.feature_extractor(prep_image)
     return reference_image, reference_features
 
-# Get initial prediction for latent
+
 def prepare_latents(latent_predictor_path=None, latent_space="WP"):
+    """Get initial prediction for latent vector."""
     if latent_predictor_path and latent_space == "WP":
         image_to_latent = InitialLatentPredictor().to(device)
         image_to_latent.load_state_dict(torch.load(latent_predictor_path))
@@ -39,8 +46,10 @@ def prepare_latents(latent_predictor_path=None, latent_space="WP"):
         initial_latents = torch.zeros((1,512)).to(device).requires_grad_(True)
     return initial_latents
 
+
 def optimize(reference_image, reference_features, initial_latents, 
-             n_iters=100, cascade=True, smoothing=None):
+             n_iters=100, cascade=True, cascade_iters=50, smoothing=None):
+    """Perform latent optimization, only on crude feature maps first few steps."""
     # Loss and optimizer
     criterion = LatentLoss()
     optimizer = torch.optim.Adam([initial_latents], lr=0.025)
@@ -62,45 +71,103 @@ def optimize(reference_image, reference_features, initial_latents,
             #avg_latents, initial_latents
         )
         loss.backward()
+
         #Optimize crude features first, then finer: zero gradients of some maps
-        if cascade and step < 5*5:
-            k_map = 3 + 2*(step//5)
-            initial_latents.grad[0, k_map:].zero_()
+        if cascade and step < cascade_iters:
+            initial_latents.grad[0, 5:].zero_()
+
         optimizer.step()
+
+        if cascade and step < cascade_iters:
+            with torch.no_grad():
+                initial_latents[0, 5:] = initial_latents[0, :5].mean(dim=0)
+
         progress_bar.set_description("Loss = {}".format(loss.item()))
 
     optimized_dlatents = initial_latents.detach().cpu().numpy()     #FIXME mb return tensors
     return optimized_dlatents
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-dlatent_path = "latents/"
-latent_predictor_path = "image_to_latent.pt"
-aligned_images = "aligned_images/"
+def arg_parse():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Latent Optimization module")
+    parser.add_argument(
+        "--root",
+        dest="root_dir",
+        help="Aligned images directory",
+        default="aligned_images/",
+        type=str,
+    )
+    parser.add_argument(
+        "--out",
+        dest="out_dir",
+        help="Latent vectors directory",
+        default="latents/",
+        type=str,
+    )
+    parser.add_argument(
+        "--predictor",
+        dest="predictor_path",
+        help="Initial predictor path",
+        default="image_to_latent.pt",
+        type=str,
+    )
+    parser.add_argument(
+        "--iters",
+        dest="n_iters",
+        help="Number of iterations",
+        default=400,
+        type=int,
+    )
+    parser.add_argument(
+        "--cascade",
+        dest="cascade_iters",
+        help="Number of first iterations, where only crude feature maps are optimized",
+        default=50,
+        type=int,
+    )
+    return parser.parse_args()
 
-# StyleGAN is converted from official tensorflow model to pytorch
-converted_model = StyleGANGenerator("stylegan_ffhq")
-avg_latents = converted_model.model.truncation.w_avg.view((-1, 1, 512))
 
-# Initialize latent optimization pipeline (generator > vgg)
-latent_optimizer = LatentOptimizer(converted_model.model, vgg_layer=12, latent_space="WP")
-
-smoothing = GaussianSmoothing(3, kernel_size=7, sigma=5).to(device)
-
-for file in os.listdir(aligned_images):
-    print(file)
-    if file.split('.')[-1] not in ["png", "jpg", "jpeg"]:
-        continue
-
-    # Load target image and get features
-    image_path = aligned_images + file
-    reference_image, reference_features = prepare_reference_image(image_path)
+def main():
+    args = arg_parse()
+    aligned_images = args.root_dir
+    dlatent_path = args.out_dir
+    use_cascade = args.cascade_iters > 0
     
-    # Predict initial latent vector or randomly sample it
-    initial_latents = prepare_latents(latent_predictor_path, latent_optimizer.latent_space)
-
-    optimized_dlatents = optimize(reference_image, reference_features, initial_latents, \
-             n_iters=200, cascade=True, smoothing=smoothing)
+    os.makedirs(args.out_dir, exist_ok=True)
     
-    # Save optimized latent vector
-    np.save(dlatent_path+image_path.split("/")[-1]+".npy", optimized_dlatents)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # StyleGAN is converted from official tensorflow model to pytorch
+    print("Loading StyleGAN, pretrained model should be in 'interfacegan/models/pretrain/'")
+    converted_model = StyleGANGenerator("stylegan_ffhq")
+
+    # Initialize latent optimization pipeline (generator -> vgg)
+    latent_optimizer = LatentOptimizer(converted_model.model, vgg_layer=12, latent_space="WP")
+
+    smoothing = GaussianSmoothing(3, kernel_size=7, sigma=5).to(device)
+    
+    print("Starting latent optimization...")
+    for file in os.listdir(aligned_images):
+        print(file)
+        if file.split('.')[-1] not in ["png", "jpg", "jpeg"]:
+            continue
+
+        # Load target image and get features
+        image_path = os.path.join(aligned_images, file)
+        reference_image, reference_features = prepare_reference_image(image_path)
+        
+        # Predict initial latent vector or randomly sample it
+        initial_latents = prepare_latents(args.predictor_path, latent_optimizer.latent_space)
+
+        optimized_dlatents = optimize(reference_image, reference_features, initial_latents, \
+            n_iters=args.n_iters, cascade=use_cascade, cascade_iters=args.cascade_iters, smoothing=smoothing)
+        
+        # Save optimized latent vector
+        dlatent_name = image_path.split("/")[-1]+".npy"
+        np.save(os.path.join(dlatent_path, dlatent_name), optimized_dlatents)
+
+
+if __name__ == "__main__":
+    main()
